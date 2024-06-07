0b19ae0cb8261735163a74fa913865f11817313f
Marquinhos <fernandez9029@gmail.com>
2024-06-07 17:04:19 -0500

    primer commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className=" border-b border-b-orange">
+      <div className="w-11/12 mx-auto max-w-[1200px] p-5 pe-10 flex justify-end items-center gap-5">
+        <a href="#" className="hover:font-black">Home</a>
+        <a href="#" className="hover:font-black">About</a>
+        <a href="#" className="hover:font-black">Projects</a>
+        <a href="#" className="hover:font-black">Contact</a>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
